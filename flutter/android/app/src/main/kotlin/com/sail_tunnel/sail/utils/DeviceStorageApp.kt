257813c81e1a4a75661f9c29b2c4885cd2a75aad
HsukqiLee <team@tsinbei.com>
2024-02-03 19:41:59 +0800

    Update DeviceStorageApp.kt

--- flutter/android/app/src/main/kotlin/com/sail_tunnel/sail/utils/DeviceStorageApp.kt
+++ flutter/android/app/src/main/kotlin/com/sail_tunnel/sail/utils/DeviceStorageApp.kt
@@ -3,9 +3,12 @@ package com.sail_tunnel.sail.utils
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import androidx.work.Configuration
+import androidx.work.WorkManager
 
 @SuppressLint("Registered")
-class DeviceStorageApp(context: Context) : Application() {
+class DeviceStorageApp(context: Context) : Application(), Configuration.Provider {
+
     init {
         attachBaseContext(context.createDeviceProtectedStorageContext())
     }
@@ -15,4 +18,10 @@ class DeviceStorageApp(context: Context) : Application() {
      * protected storage.
      */
     override fun getApplicationContext() = this
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setMinimumLoggingLevel(android.util.Log.INFO)
+            .build()
+    }
 }
