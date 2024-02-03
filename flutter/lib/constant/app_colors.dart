8b01360e4e4d760a95a1012d2baf85ae8f248d75
HsukqiLee <team@tsinbei.com>
2024-02-03 21:23:50 +0800

    Update app_colors.dart

--- flutter/lib/constant/app_colors.dart
+++ flutter/lib/constant/app_colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const MaterialColor themeColor = Colors.blue; // 改变这里
-  static const Color yellowColor = Colors.blue.shade400; // 改变这里
+  static const MaterialColor themeColor = Colors.green; // Colors.amber;
+  static const Color yellowColor = Colors.green; // Color(0xFFfbd033);
   static const Color grayColor = Color(0xFF2d2d2d);
   static const Color darkSurfaceColor = Color(0xff373737);
 
   static const Color whiteColor = Colors.white;
-  static const MaterialColor greenColor = Colors.blue; // 改变这里
+  static const MaterialColor greenColor = Colors.green;
 }
